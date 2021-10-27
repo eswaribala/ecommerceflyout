0a19575c6841b9956753e19f67a5c5560e5d07fa
eswaribala <parameswaribala@gmail.com>
2021-10-27 21:47:21 +0530

    updated

---
+++ ecommerceflyout/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ecommerceflyout.ViewModels
+{
+    public class RegistrationViewModel:BaseViewModel
+    {
+        public RegistrationViewModel()
+        {
+            Title = "Customer Registration";
+        }
+    }
+}
