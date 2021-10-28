76d2e43f697d321f3b6b9516191117ecf8455e11
eswaribala <parameswaribala@gmail.com>
2021-10-28 21:56:28 +0530

    updated

---
+++ ecommerceflyout/ViewModels/NetworkPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ecommerceflyout.ViewModels
+{
+    public class NetworkPageViewModel:BaseViewModel
+    {
+
+        public NetworkPageViewModel()
+        {
+            Title = "Connectivity Test";
+        }
+    }
+}
---
+++ ecommerceflyout/Views/NetworkPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ecommerceflyout.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NetworkPage : ContentPage
+    {
+
+        private NetworkAccess CurrentAccess;
+        private IEnumerable<ConnectionProfile> Profiles;
+
+
+        public NetworkPage()
+        {
+            InitializeComponent();
+
+            CurrentAccess = Connectivity.NetworkAccess;
+
+            Profiles = Connectivity.ConnectionProfiles;
+            //tracing dynamic changes in network
+            Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+            if(CurrentAccess == NetworkAccess.Internet)
+            {
+                NetworkType.Text = "Available Network is Internet";
+            }
+            if (CurrentAccess == NetworkAccess.Local)
+            {
+                NetworkType.Text = "Available Network is Local";
+            }
+
+            if (Profiles.Contains(ConnectionProfile.WiFi))
+            {
+                NetworkTProfile.Text = "Network Profile is WIFI";
+            }
+            if (Profiles.Contains(ConnectionProfile.Ethernet))
+            {
+                NetworkTProfile.Text = "Network Profile is Ethernet";
+            }
+
+        }
+
+        private void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            CurrentAccess = Connectivity.NetworkAccess;
+            Profiles= Connectivity.ConnectionProfiles;
+        }
+    }
+}
