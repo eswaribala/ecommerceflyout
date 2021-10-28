1cbb7301fc59f5eb1ac811af9d6cf35ef30b6b47
eswaribala <parameswaribala@gmail.com>
2021-10-28 23:06:59 +0530

    updated

---
+++ ecommerceflyout/ViewModels/ShootPhotoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ecommerceflyout.ViewModels
+{
+    public class ShootPhotoViewModel:BaseViewModel
+    {
+       
+        public ShootPhotoViewModel()
+        {
+            Title = "Live Photo ";
+        }
+    }
+}
---
+++ ecommerceflyout/Views/ShootPhotoPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ecommerceflyout.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShootPhotoPage : ContentPage
+    {
+        private string FullPath = null;
+        public ShootPhotoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PhotoClick_Pressed(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private async void Phototask()
+        {
+            try
+            {
+
+                var Photo = MediaPicker.CapturePhotoAsync();
+            }
+            catch(FeatureNotEnabledException fx)
+            {
+
+            }
+            catch(PermissionException pEx)
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+
+        }
+
+        async Task LoadPhotoAsync(FileResult Photo)
+        {
+            if(Photo == null)
+            {
+                FullPath = null;
+            }
+            else
+            {
+                var NewFile = Path.Combine(FileSystem.CacheDirectory, Photo.FileName);
+                using (var stream = await Photo.OpenReadAsync())
+                using (var newStream = File.OpenWrite(NewFile))
+                    await stream.CopyToAsync(newStream);
+                FullPath = NewFile;
+            }
+
+
+        }
+
+    }
+}
