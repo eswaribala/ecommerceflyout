50e3531241199908f8a143fc125eed802c65700c
eswaribala <parameswaribala@gmail.com>
2021-10-29 00:02:23 +0530

    updated

---
+++ ecommerceflyout/Themes/ThemeManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ecommerceflyout.Themes
+{
+    public class ThemeManager
+    {
+        public static void ChangeTheme(string themeName)
+        {
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Clear();
+            var type = typeof(ThemeManager);
+            var uri = $"{type.Assembly.GetName().Name}.Themes.{themeName}";
+            var theme = Type.GetType(uri);
+            Application.Current.Resources.MergedWith = theme;
+        }
+    }
+}
