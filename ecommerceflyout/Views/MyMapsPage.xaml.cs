2bcc7466262e92b4a1659504db59b9c817aa74ef
eswaribala <parameswaribala@gmail.com>
2021-10-29 22:43:34 +0530

    updated

--- ecommerceflyout/Views/MyMapsPage.xaml.cs
+++ ecommerceflyout/Views/MyMapsPage.xaml.cs
@@ -30,14 +30,32 @@ namespace ecommerceflyout.Views
                 if (yourLocation == null)
                     await DisplayAlert("Attention", "GPS is not available", "Ok");
                 else
+                {
                     Latitude.Text = $"{yourLocation.Latitude}";
                     Longitude.Text = $"{yourLocation.Longitude}";
+                }
             }
             catch (Exception ex)
             {
                 await DisplayAlert("Ooops!", $"Something went wrong: {ex.Message}", "Ok");
             }
 
+        }
+
+        private async void Map_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Map.OpenAsync(double.Parse(Latitude.Text), double.Parse(Longitude.Text), new MapLaunchOptions
+                {
+                    Name = "My location",
+                    NavigationMode = NavigationMode.None
+                });
+            }
+            catch (Exception ex1)
+            {
+                await DisplayAlert("Ooops!", $"Remenber to search your coordinates, first.", "Ok");
+            }
         }
     }
 }
