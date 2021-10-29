5cfead71d98dbff25cb132a41d0e41c8e1173632
eswaribala <parameswaribala@gmail.com>
2021-10-29 22:04:42 +0530

    updated

---
+++ ecommerceflyout/ViewModels/MyMapsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ecommerceflyout.ViewModels
+{
+    public class MyMapsViewModel:BaseViewModel
+    {
+        public MyMapsViewModel()
+        {
+            Title = "Identify My Location";
+        }
+    }
+}
---
+++ ecommerceflyout/Views/MyMapsPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ecommerceflyout.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyMapsPage : ContentPage
+    {
+        public MyMapsPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void MyLocation_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var yourLocation = await Geolocation.GetLocationAsync(new GeolocationRequest()
+                {
+                    DesiredAccuracy = GeolocationAccuracy.Medium,
+                    Timeout = TimeSpan.FromSeconds(30)
+                });
+
+                if (yourLocation == null)
+                    await DisplayAlert("Attention", "GPS is not available", "Ok");
+                else
+                    Latitude.Text = $"{yourLocation.Latitude}";
+                    Longitude.Text = $"{yourLocation.Longitude}";
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ooops!", $"Something went wrong: {ex.Message}", "Ok");
+            }
+
+        }
+    }
+}
